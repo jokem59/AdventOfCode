8a71518d4ad8ca2004c824e6e9ab344f87d5b7f1
JOEKIM\joeki <joe.kim509@gmail.com>
2018-12-13 03:24:37 -0800

    started 1-2

---
+++ 2018/day1/day1-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+int main(int argc, char* argv[]) {
+    std::ifstream input(argv[1]);
+    std::string line;
+    int frequency = 0;
+    std::string::size_type sz;
+
+    if (input.is_open()) {
+        while (std::getline(input, line)) {
+            if (line[0] == '-') {
+                frequency -= std::stoi(line.substr(1,line.size()), &sz);
+            } else {
+                frequency += std::stoi(line.substr(1, line.size()), &sz);
+            }
+        }
+
+        input.close();
+    }
+
+    std::cout << "Remaining Frequency: " << frequency << '\n';
+
+    return 0;
+}
