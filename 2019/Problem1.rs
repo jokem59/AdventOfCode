d352d2ec8e280d0daaaf1fa1210ea62b8b7c1308
Joseph Kim <joskim@microsoft.com>
2019-12-06 08:40:57 -0800

    starting aoc 2019 in rust

---
+++ 2019/Problem1.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+
+fn main() -> io::Result<()> {
+    let file = File::open("Input1.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut val = 0;
+    let mut sum = 0;
+    for line in reader.lines() {
+        match line {
+            Ok(v) => val = v.parse::<u32>().unwrap(),
+            Err(e) => println!("Error parsing line: {:?}", e),
+        }
+
+        sum += (val / 3) - 2;
+    }
+
+    println!("Total: {}", sum);
+
+    Ok(())
+}
